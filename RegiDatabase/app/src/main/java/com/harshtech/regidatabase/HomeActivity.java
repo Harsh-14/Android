b1acbc6fc991d4b2d9020f60ce9a60939d679968
Harsh-14 <harshhp948@gmail.com>
2022-02-05 11:42:12 +0530

    databasefinal

--- RegiDatabase/app/src/main/java/com/harshtech/regidatabase/HomeActivity.java
+++ RegiDatabase/app/src/main/java/com/harshtech/regidatabase/HomeActivity.java
@@ -2,13 +2,30 @@ package com.harshtech.regidatabase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
-
+    TextView name;
+    TextView email;
+    TextView address;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        name=findViewById(R.id.textView5);
+        email=findViewById(R.id.textView6);
+        address=findViewById(R.id.textView7);
+
+        Cursor cr=new DBmanager(getApplicationContext()).fetchdata(getIntent().getStringExtra("name"));
+        while (cr.moveToNext()){
+            name.setText(cr.getString(1));
+            email.setText(cr.getString(4));
+            address.setText(cr.getString(2));
+        }
     }
 }
--- RegiDatabase/app/src/main/java/com/harshtech/regidatabase/LoginActivity.java
+++ RegiDatabase/app/src/main/java/com/harshtech/regidatabase/LoginActivity.java
@@ -2,8 +2,10 @@ package com.harshtech.regidatabase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
@@ -18,15 +20,22 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         EditText name=findViewById(R.id.editTextTextPersonName5);
-        EditText mobile=findViewById(R.id.editTextTextPersonName6);
+        EditText pass=findViewById(R.id.editTextTextPersonName6);
         findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Intent intent=new Intent(LoginActivity.this,HomeActivity.class);
                 Cursor cr=new DBmanager(getApplicationContext()).fetchdata(name.getText().toString());
                 Toast.makeText(getApplicationContext(),"Hello Login",Toast.LENGTH_SHORT).show();
-                Log.d("harsh", cr.getString((int)cr.getColumnIndex("NAME")));
-
-//                Toast.makeText(getApplicationContext(),cr.getColumnIndex("password"),Toast.LENGTH_SHORT).show();
+                while (cr.moveToNext()){
+                    if(pass.getText().toString().equals(cr.getString(6))){
+                        Log.d("harsh",cr.getString(6)+" "+cr.getString(1));
+                        intent.putExtra("name",name.getText().toString());
+                        startActivity(intent);
+                    }
+                    else
+                        Toast.makeText(getApplicationContext(),"Wrong Password",Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
