337abb29df893f73fb0623d5a047b3ed6edcfa9d
Harsh-14 <harshhp948@gmail.com>
2022-02-05 10:46:28 +0530

    recyfinal

--- RecyclerView/app/src/main/java/com/harshtech/recyclerview/MainActivity.java
+++ RecyclerView/app/src/main/java/com/harshtech/recyclerview/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.SearchView;
@@ -16,6 +17,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     SearchView searchView;
+    RecyAdapter recyAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,21 +27,22 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         List<String> data=new ArrayList<String>();
-        data.add("CPP");
-        data.add("Python");
-        data.add("java");
-        data.add("Kotlin");
-        data.add("CPP");
-        data.add("Python");
-        data.add("java");
-        data.add("Kotlin");
-        data.add("CPP");
-        data.add("Python");
-        data.add("java");
-        data.add("Kotlin");
+        data.add("C++ is a cross-platform language that can be used to create high-performance applications. C++ was developed by Bjarne Stroustrup");
+        data.add("Python is an interpreted, object-oriented, high-level programming language with dynamic semantics.");
+        data.add("Java is a programming language and computing platform first released by Sun Microsystems in 1995");
+        data.add("Kotlin is a cross-platform, statically typed, general-purpose programming language with type inference. Kotlin is designed to interoperate fully with Java");
+        data.add("C++ is a cross-platform language that can be used to create high-performance applications. C++ was developed by Bjarne Stroustrup");
+        data.add("Python is an interpreted, object-oriented, high-level programming language with dynamic semantics.");
+        data.add("Java is a programming language and computing platform first released by Sun Microsystems in 1995");
+        data.add("Kotlin is a cross-platform, statically typed, general-purpose programming language with type inference. Kotlin is designed to interoperate fully with Java");
+        data.add("C++ is a cross-platform language that can be used to create high-performance applications. C++ was developed by Bjarne Stroustrup");
+        data.add("Python is an interpreted, object-oriented, high-level programming language with dynamic semantics.");
+        data.add("Java is a programming language and computing platform first released by Sun Microsystems in 1995");
+        data.add("Kotlin is a cross-platform, statically typed, general-purpose programming language with type inference. Kotlin is designed to interoperate fully with Java");
 
         recyclerView=findViewById(R.id.recy_view);
-        recyclerView.setAdapter(new RecyAdapter(data));
+        recyAdapter=new RecyAdapter(data);
+        recyclerView.setAdapter(recyAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
@@ -49,7 +52,19 @@ public class MainActivity extends AppCompatActivity {
 
         MenuItem searchItem = menu.findItem(R.id.action_search);
         searchView = (SearchView) searchItem.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
 
+            @Override
+            public boolean onQueryTextChange(String s) {
+                Log.d("harsh",s);
+                recyAdapter.getFilter().filter(s);
+                return false;
+            }
+        });
         return super.onCreateOptionsMenu(menu);
     }
 }
--- RecyclerView/app/src/main/java/com/harshtech/recyclerview/RecyAdapter.java
+++ RecyclerView/app/src/main/java/com/harshtech/recyclerview/RecyAdapter.java
@@ -3,18 +3,23 @@ package com.harshtech.recyclerview;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class RecyAdapter extends RecyclerView.Adapter<RecyAdapter.RecyHolder> {
+public class RecyAdapter extends RecyclerView.Adapter<RecyAdapter.RecyHolder> implements Filterable {
     List<String> data;
+    List<String> backup;
     public RecyAdapter(List<String> data){
         this.data=data;
+        this.backup=new ArrayList<>(data);
     }
     @NonNull
     @Override
@@ -31,6 +36,38 @@ public class RecyAdapter extends RecyclerView.Adapter<RecyAdapter.RecyHolder> {
     @Override
     public int getItemCount() {
         return data.size();
+    }
+
+
+
+    Filter filter=new Filter() {
+        @Override
+        protected FilterResults performFiltering(CharSequence charSequence) {
+            ArrayList<String> filterdata=new ArrayList<String>();
+            if(charSequence.toString().isEmpty())
+                filterdata.addAll(backup);
+            else
+                for (String dat:backup)
+                    if(dat.toString().toLowerCase().contains(charSequence.toString().toLowerCase()))
+                        filterdata.add(dat);
+
+            FilterResults filterResults=new FilterResults();
+            filterResults.values=filterdata;
+            return filterResults;
+        }
+
+        @Override
+        protected void publishResults(CharSequence charSequence, @NonNull FilterResults filterResults) {
+            data.clear();
+            data.addAll( (ArrayList<String>) filterResults.values);
+
+            notifyDataSetChanged();
+        }
+    };
+
+    @Override
+    public Filter getFilter() {
+        return filter;
     }
 
     class RecyHolder extends RecyclerView.ViewHolder{
